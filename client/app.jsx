77ff35a8ad97318d215c3068cfc338128c74ebc2
Hannah Engelhardt <hannahnengel@gmail.com>
2023-01-31 23:30:08 +0000

    Create a 404 not found page  Update App component to use parseRoute for hashrouting

--- client/app.jsx
+++ client/app.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
+import { parseRoute } from './lib';
 import Home from './pages/home';
 import PageContainer from './components/page-container';
 import Navbar from './components/navbar';
-
+import NotFound from './pages/not-found';
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+    onhashchange = event => {
+      this.setState({ route: parseRoute(window.location.hash) });
+    };
+  }
+
+  renderPage() {
+    const { route } = this.state;
+
+    if (route.path === '') {
+      return <Home />;
+    }
+    return <NotFound />;
+  }
+
   render() {
     return (
       <>
         <Navbar />
         <PageContainer>
-          <Home />
+          { this.renderPage() }
         </PageContainer>
       </>
     );
