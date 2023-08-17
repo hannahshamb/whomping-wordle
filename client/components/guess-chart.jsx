b2f11718d800ed27c04ebc9775ef21ba59959afb
Hannah Engelhardt <hannahnengel@gmail.com>
2023-08-17 20:31:25 +0000

    Refactor some of the guess-chart code to be more modular

--- client/components/guess-chart.jsx
+++ client/components/guess-chart.jsx
@@ -31,7 +31,7 @@ export default function GuessChart(props) {
 
   }, [displayedColumns, guesses, targetRow]);
 
-  const headers = ['Character', 'Gender', 'Hair Colour', 'House', 'Role', 'Species', 'Ancestry', 'Alive'];
+  const headers = ['character', 'gender', 'hairColour', 'role', 'house', 'species', 'ancestry', 'alive'];
   return (
     <>
       <div className="scroll-container mt-1 p-0 w-100" ref={scrollContainerRef}>
@@ -39,8 +39,14 @@ export default function GuessChart(props) {
           <thead>
             <tr className='d-flex justify-content-center'>
               {headers.map((header, index) => {
+                if (header === 'hairColour') {
+                  return <th key={index}>Hair Colour</th>;
+                }
+                if (header === 'role') {
+                  return <th key={index}>Hogwarts</th>;
+                }
                 return (
-                  <th key={index}>{header}</th>
+                  <th key={index}>{header[0].toUpperCase() + header.slice(1)}</th>
                 );
               })}
             </tr>
@@ -50,73 +56,7 @@ export default function GuessChart(props) {
             {guesses.slice(0).reverse().map((guess, rowIndex) => {
               rowKey--;
 
-              let genderClass = 'red';
-              let hairColourClass = 'red';
-              let houseClass = 'red';
-              let roleClass = 'red';
-              let speciesClass = 'red';
-              let ancestryClass = 'red';
-              let aliveClass = 'red';
-              let role;
               const tds = [];
-              const compare = (guess, characterOfTheDay) => {
-                // gender comparison
-                if (guess.characterData.gender === characterOfTheDay.gender) {
-                  genderClass = 'green';
-                }
-
-                // hair colour comparison
-                if ((guess.characterData.hairColour === 'blonde' || guess.characterData.hairColour === 'blond') &&
-                (characterOfTheDay.hairColour === 'blonde' || characterOfTheDay.hairColour === 'blond')) {
-                  hairColourClass = 'green';
-                } else if (guess.characterData.hairColour === characterOfTheDay.hairColour) {
-                  hairColourClass = 'green';
-                }
-
-                // house comparison
-                if (guess.characterData.house === characterOfTheDay.house) {
-                  houseClass = 'green';
-                }
-
-                // role comparison
-                if (guess.characterData.hogwartsStudent === true) {
-                  role = 'Student';
-                } else if (guess.characterData.hogwartsStaff === true) {
-                  role = 'Staff';
-                } else {
-                  role = 'Other';
-                }
-                let characterOfTheDayRole;
-                if (characterOfTheDay.hogwartsStudent === true) {
-                  characterOfTheDayRole = 'Student';
-                } else if (characterOfTheDay.hogwartsStaff === true) {
-                  characterOfTheDayRole = 'Staff';
-                } else {
-                  characterOfTheDayRole = 'Other';
-                }
-                if (role === characterOfTheDayRole) {
-                  roleClass = 'green';
-                }
-
-                // species comparison
-                if (guess.characterData.species === characterOfTheDay.species) {
-                  speciesClass = 'green';
-                }
-
-                // ancestry comparison
-                if (guess.characterData.ancestry === characterOfTheDay.ancestry) {
-                  ancestryClass = 'green';
-                }
-
-                // alive comparison
-                if (guess.characterData.alive === characterOfTheDay.alive) {
-                  aliveClass = 'green';
-                }
-
-              };
-
-              compare(guess, characterOfTheDay);
-
               let imgDetails =
                 (<div className="category-img-container">
                   <img className='character-img-wizard' src='../imgs/Wizard-Purple.png' alt={`${guess.characterData.name}`} />
@@ -127,103 +67,63 @@ export default function GuessChart(props) {
                     <img className='character-img-lg' src={`${guess.characterData.image}`} alt={`${guess.characterData.name}`} />
                   </div>;
               }
-
-              let hairColour;
-              if (guess.characterData.hairColour === '') {
-                hairColour = 'Unknown';
-              } else {
-                hairColour = guess.characterData.hairColour[0].toUpperCase() + guess.characterData.hairColour.slice(1);
-              }
-
-              let house;
-              if (guess.characterData.house === '') {
-                house = 'Unknown';
-              } else {
-                house = guess.characterData.house[0].toUpperCase() + guess.characterData.house.slice(1);
-              }
-
-              let species;
-              if (guess.characterData.species === '') {
-                species = 'Unknown';
-              } else {
-                species = guess.characterData.species[0].toUpperCase() + guess.characterData.species.slice(1);
-              }
-
-              let ancestry;
-              if (guess.characterData.ancestry === '') {
-                ancestry = 'Unknown';
-              } else {
-                ancestry = guess.characterData.ancestry[0].toUpperCase() + guess.characterData.ancestry.slice(1);
-              }
-
-              let alive;
-              if (guess.characterData.alive) {
-                alive = 'True';
-              } else {
-                alive = 'False';
-              }
               tds.push({
+                thName: 'Character',
                 imgDetails,
                 classColor: '',
                 p: guess.characterData.name
               });
-              tds.push({
-                imgDetails: null,
-                classColor: genderClass,
-                p: guess.characterData.gender[0].toUpperCase() + guess.characterData.gender.slice(1)
-              });
-              tds.push({
-                imgDetails: null,
-                classColor: hairColourClass,
-                p: hairColour
-              });
-              tds.push({
-                imgDetails: null,
-                classColor: houseClass,
-                p: house
-              });
-              tds.push({
-                imgDetails: null,
-                classColor: roleClass,
-                p: role
-              });
-              tds.push({
-                imgDetails: null,
-                classColor: speciesClass,
-                p: species
-              });
-              tds.push({
-                imgDetails: null,
-                classColor: ancestryClass,
-                p: ancestry
-              });
-              tds.push({
-                imgDetails: null,
-                classColor: aliveClass,
-                p: alive
+
+              for (const key in characterOfTheDay) {
+                let tdClassColor;
+                if (key !== 'image' && key !== 'name' && key !== 'id') {
+                  if (key === 'hairColour' && ((guess.characterData[key] === 'blonde' || guess.characterData[key] === 'blond') &&
+                    (characterOfTheDay[key] === 'blonde' || characterOfTheDay[key] === 'blond'))) {
+                    tdClassColor = 'green';
+                  } else {
+                    if (characterOfTheDay[key] === guess.characterData[key]) {
+                      tdClassColor = 'green';
+                    } else if (characterOfTheDay[key] !== guess.characterData[key]) {
+                      tdClassColor = 'red';
+                    }
+                  }
+                  tds.push({
+                    thName: key,
+                    classColor: tdClassColor,
+                    p: guess.characterData[key][0].toUpperCase() + guess.characterData[key].slice(1)
+                  });
+                }
+              }
+
+              const itemPositions = {};
+              for (const [index, thName] of headers.entries()) {
+                itemPositions[thName] = index;
+              }
+              tds.sort((a, b) => {
+                return itemPositions[a.thName] - itemPositions[b.thName];
               });
 
               return (
                 <tr key={rowKey} className='d-flex justify-content-center'>
                   {
-                    tds.map((cell, cellIndex) => {
-                      return (
+                        tds.map((cell, cellIndex) => {
+                          return (
 
-                        <td key={cellIndex} className={
-                          rowKey !== targetRow
-                            ? ''
-                            : displayedColumns.includes(cellIndex) ? 'show' : 'hidden'
-                          }>
-                          <div className='position-relative'>
-                            {cell.imgDetails ? <div> {cell.imgDetails} </div> : <div className={`category-box ${cell.classColor}`} />}
-                            <div className="overlay">
-                              <p className='td-font'>{cell.p}</p>
-                            </div>
-                          </div>
-                        </td>
-                      );
-                    })
-                  }
+                            <td key={cellIndex} className={
+                              rowKey !== targetRow
+                                ? ''
+                                : displayedColumns.includes(cellIndex) ? 'show' : 'hidden'
+                            }>
+                              <div className='position-relative'>
+                                {cell.imgDetails ? <div> {cell.imgDetails} </div> : <div className={`category-box ${cell.classColor}`} />}
+                                <div className="overlay">
+                                  <p className='td-font'>{cell.p}</p>
+                                </div>
+                              </div>
+                            </td>
+                          );
+                        })
+                      }
                 </tr>
               );
             })}
