6cac5861f3daff0c512cea80b7514e4330538789
Hannah Engelhardt <hannahnengel@gmail.com>
2023-08-22 18:49:11 +0000

    Refactor and move the guess-chart component code to live in the game-form component to track game status based on colors from user guesses

--- client/components/game-form.jsx
+++ client/components/game-form.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Select from 'react-select';
 import CharacterOfTheDay from './character-of-the-day';
-// import GuessChart from './guess-chart';
 import Legend from './legend';
 import CheckGuesses from './check-guesses';
 import { AppContext } from '../lib';
@@ -17,9 +16,11 @@ export default class GameForm extends React.Component {
       guessesRemaining: null,
       error: false,
       gameStatus: null,
-      displayedColumns: []
+      displayedColumns: [],
+      forcedFeit: false
     };
     this.scrollContainerRef = React.createRef();
+    this.animationTimeoutRef = null;
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleContinue = this.handleContinue.bind(this);
@@ -48,7 +49,9 @@ export default class GameForm extends React.Component {
     if (guessesRemaining <= 0) {
       guessesRemaining = 0;
     }
-    this.setState({ characterData: {}, guesses, today, guessesRemaining });
+    const targetRow = guesses.length - 1;
+    this.setState({ characterData: {}, guesses, today, guessesRemaining, targetRow });
+    this.clearAnimation();
   }
 
   scrollLeft = () => {
@@ -59,13 +62,20 @@ export default class GameForm extends React.Component {
     this.scrollContainerRef.current.scrollLeft += 100;
   };
 
+  clearAnimation() {
+    clearTimeout(this.animationTimeoutRef);
+    this.setState({ displayedColumns: [] });
+    this.animationTimeoutRef = null;
+  }
+
   handleChange(event) {
     const { characterData } = event;
+    this.clearAnimation();
     this.setState({ characterData, error: false });
   }
 
   handleContinue() {
-    this.setState({ gameStatus: 'lose' });
+    this.setState({ gameStatus: 'lose', forcedForfeit: true });
   }
 
   componentDidMount() {
@@ -79,19 +89,60 @@ export default class GameForm extends React.Component {
     if (guessesRemaining <= 0) {
       guessesRemaining = 0;
     }
+    let forcedForfeit = false;
+    if (guessesRemaining === 0) {
+      forcedForfeit = true;
+    }
     this.setState({
       characters: characterData,
       characterOfTheDay,
       guesses,
       guessesRemaining,
-      targetRow
+      targetRow,
+      forcedForfeit
     });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      this.state.targetRow !== this.state.guesses.length ||
+      prevState.displayedColumns.length !== this.state.displayedColumns.length
+    ) {
+      this.updateDisplayedColumns();
+    }
+    if (prevState.characterData !== this.state.characterData) {
+      this.clearAnimation();
+    }
+  }
+
+  updateDisplayedColumns = () => {
+    const { targetRow, displayedColumns, guesses } = this.state;
+
+    if (targetRow !== guesses.length) {
+      if (displayedColumns.length <= 7) {
+        this.animationTimeoutRef = setTimeout(() => {
+          this.setState(prevState => ({
+            displayedColumns: [...prevState.displayedColumns, prevState.displayedColumns.length]
+          }));
+        }, 500);
+        return () => clearTimeout(this.animationTimeoutRef);
+      } else {
+        this.setState(prevState => ({
+          displayedColumns: [],
+          targetRow: prevState.targetRow + 1
+        }));
+      }
+    }
+  };
+
+  componentWillUnmount() {
+    this.clearAnimation();
   }
 
   render() {
     const {
       characterData, error, guesses, characters, characterOfTheDay,
-      guessesRemaining, gameStatus, displayedColumns, targetRow
+      guessesRemaining, gameStatus, displayedColumns, targetRow, forcedForfeit
     } = this.state;
     const errorClass = error ? '' : 'd-none';
 
@@ -175,7 +226,6 @@ export default class GameForm extends React.Component {
 
     const headers = ['character', 'gender', 'hairColour', 'role', 'house', 'species', 'ancestry', 'alive'];
     let rowKey = guesses.length;
-    // console.log('guesses', guesses);
     const guessChart = (
       <>
         <div className="scroll-container mt-1 p-0 w-100" ref={this.scrollContainerRef}>
@@ -285,15 +335,13 @@ export default class GameForm extends React.Component {
       </>
     );
 
-    // <GuessChart guesses={guesses} characterOfTheDay={characterOfTheDay} guessesRemaining={guessesRemainingClass} gameStatus={gameStatus} />;
-
     return (
       <>
         {gameStatus === 'lose'
           ? <p>FORFEIT</p>
           : gameStatus === 'win'
             ? <p>YOU WON!!!</p>
-            : guessesRemaining === 0
+            : guessesRemaining === 0 && !forcedForfeit
               ? <>
                 <div className="row justify-content-center mt-3 w-100">
                   <p className='guesses-font'>Guesses remaining: <span className={`guesses-font ${guessesRemainingClass}`}>{guessesRemaining}</span></p>
@@ -303,9 +351,11 @@ export default class GameForm extends React.Component {
                 {/* Forfeit button - will need to send character of the day as well */}
                 <Legend />
               </>
-              : <>
-                <div className="row position-relative" style={{ width: '500px' }}>
-                  <Select
+              : forcedForfeit
+                ? <p>FORFEIT</p>
+                : <>
+                  <div className="row position-relative" style={{ width: '500px' }}>
+                    <Select
                     className='w-100 mx-2 text-left'
                     placeholder={`${placeholder}`}
                     options={mappedOptions}
@@ -318,29 +368,29 @@ export default class GameForm extends React.Component {
                     onChange={this.handleChange}
                     noOptionsMessage={() => 'No characters with that name...'}
                   />
-                  <div className="btn-absolute mx-2">
-                    <button className='white-btn form-font' style={{ width: '100px', height: '72px' }} onClick={this.handleSubmit}>
-                      <i className="fa-lg fa-sharp fa-solid fa-wand-sparkles" />
-                    </button>
+                    <div className="btn-absolute mx-2">
+                      <button className='white-btn form-font' style={{ width: '100px', height: '72px' }} onClick={this.handleSubmit}>
+                        <i className="fa-lg fa-sharp fa-solid fa-wand-sparkles" />
+                      </button>
+                    </div>
                   </div>
-                </div>
-                <div className={`row ${errorClass} justify-content-center mt-3 w-100`}>
-                  <p className='error-font'>Must select a correct character name from the provided list</p>
-                </div>
-                <div className="row justify-content-center mt-3 w-100">
-                  <p className='guesses-font'>Guesses remaining: <span className={`guesses-font ${guessesRemainingClass}`}>{guessesRemaining}</span></p>
-                </div>
-                {guesses && guesses.length > 0
-                  ? <>
-                    {guessChart}
-                    {/* Forfeit button - will need to send character of the day as well */}
-                    <Legend />
-                  </>
-                  : <p>This happens when there are not guesses yet</p>
+                  <div className={`row ${errorClass} justify-content-center mt-3 w-100`}>
+                    <p className='error-font'>Must select a correct character name from the provided list</p>
+                  </div>
+                  <div className="row justify-content-center mt-3 w-100">
+                    <p className='guesses-font'>Guesses remaining: <span className={`guesses-font ${guessesRemainingClass}`}>{guessesRemaining}</span></p>
+                  </div>
+                  {guesses && guesses.length > 0
+                    ? <>
+                      {guessChart}
+                      {/* Forfeit button - will need to send character of the day as well */}
+                      <Legend />
+                    </>
+                    : null
                 }
-              </>
+                </>
       }
-        {/* comment */}
+        {/*  */}
       </>
 
     );
@@ -348,177 +398,3 @@ export default class GameForm extends React.Component {
 
 }
 GameForm.contextType = AppContext;
-
-/*
-  const { guesses, characterOfTheDay, gameStatus } = props;
-  const scrollContainerRef = useRef(null);
-  const [displayedColumns, setDisplayedColumns] = useState([]);
-  let rowKey = guesses.length;
-  const [targetRow, setTargetRow] = useState(guesses.length - 1);
-
-  const scrollLeft = () => {
-    scrollContainerRef.current.scrollLeft -= 100;
-  };
-
-  const scrollRight = () => {
-    scrollContainerRef.current.scrollLeft += 100;
-  };
-
-  useEffect(() => {
-    if (targetRow !== guesses.length) {
-      if (displayedColumns.length <= 7) {
-        const timeout = setTimeout(() => {
-          setDisplayedColumns(prevColumns => [...prevColumns, displayedColumns.length]);
-        }, 500);
-        return () => clearTimeout(timeout);
-      } else {
-        setDisplayedColumns([]);
-        setTargetRow(prevTarget => prevTarget + 1);
-      }
-
-    }
-
-  }, [displayedColumns, guesses, targetRow]);
-
-  const headers = ['character', 'gender', 'hairColour', 'role', 'house', 'species', 'ancestry', 'alive'];
-  // const colorMap = (guesses, characterOfTheDay) => {
-  //   const colorMap = [];
-  //   guesses.forEach(guess => {
-  //     const colors = [];
-  //     for (const key in characterOfTheDay) {
-  //       if (key !== 'image' && key !== 'name' && key !== 'id') {
-  //         if (key === 'hairColour' && ((guess.characterData[key] === 'blonde' || guess.characterData[key] === 'blond') &&
-  //           (characterOfTheDay[key] === 'blonde' || characterOfTheDay[key] === 'blond'))) {
-  //           colors.push({ thName: key, color: 'green' });
-  //         } else {
-  //           if (characterOfTheDay[key] === guess.characterData[key]) {
-  //             colors.push({ thName: key, color: 'green' });
-  //           } else if (characterOfTheDay[key] !== guess.characterData[key]) {
-  //             colors.push({ thName: key, color: 'red' });
-  //           }
-  //         }
-  //       }
-  //     }
-  //     const itemPositions = {};
-  //     for (const [index, thName] of headers.entries()) {
-  //       itemPositions[thName] = index;
-  //     }
-  //     colors.sort((a, b) => {
-  //       return itemPositions[a.thName] - itemPositions[b.thName];
-  //     });
-  //     colorMap.push({ guessNumber: guess.guessNumber, colors });
-  //   });
-  //   return colorMap;
-  // };
-  return (
-    gameStatus === 'lose'
-      ? null
-      // colorMap(guesses, characterOfTheDay)
-      : <>
-        <div className="scroll-container mt-1 p-0 w-100" ref={scrollContainerRef}>
-          <table cellSpacing={0} cellPadding={0}>
-            <thead>
-              <tr className='d-flex justify-content-center'>
-                {headers.map((header, index) => {
-                  if (header === 'hairColour') {
-                    return <th key={index}>Hair Colour</th>;
-                  }
-                  if (header === 'role') {
-                    return <th key={index}>Hogwarts</th>;
-                  }
-                  return (
-                    <th key={index}>{header[0].toUpperCase() + header.slice(1)}</th>
-                  );
-                })}
-              </tr>
-            </thead>
-
-            <tbody>
-              {guesses.slice(0).reverse().map((guess, rowIndex) => {
-                rowKey--;
-
-                const tds = [];
-                let imgDetails =
-                (<div className="category-img-container">
-                  <img className='character-img-wizard' src='../imgs/Wizard-Purple.png' alt={`${guess.characterData.name}`} />
-                </div>);
-                if (guess.characterData.image !== '') {
-                  imgDetails =
-                    <div className="category-img-container">
-                      <img className='character-img-lg' src={`${guess.characterData.image}`} alt={`${guess.characterData.name}`} />
-                    </div>;
-                }
-                tds.push({
-                  thName: 'Character',
-                  imgDetails,
-                  classColor: '',
-                  p: guess.characterData.name
-                });
-
-                for (const key in characterOfTheDay) {
-                  let tdClassColor;
-                  if (key !== 'image' && key !== 'name' && key !== 'id') {
-                    if (key === 'hairColour' && ((guess.characterData[key] === 'blonde' || guess.characterData[key] === 'blond') &&
-                    (characterOfTheDay[key] === 'blonde' || characterOfTheDay[key] === 'blond'))) {
-                      tdClassColor = 'green';
-                    } else {
-                      if (characterOfTheDay[key] === guess.characterData[key]) {
-                        tdClassColor = 'green';
-                      } else if (characterOfTheDay[key] !== guess.characterData[key]) {
-                        tdClassColor = 'red';
-                      }
-                    }
-                    tds.push({
-                      thName: key,
-                      classColor: tdClassColor,
-                      p: guess.characterData[key][0].toUpperCase() + guess.characterData[key].slice(1)
-                    });
-                  }
-                }
-
-                const itemPositions = {};
-                for (const [index, thName] of headers.entries()) {
-                  itemPositions[thName] = index;
-                }
-                tds.sort((a, b) => {
-                  return itemPositions[a.thName] - itemPositions[b.thName];
-                });
-
-                return (
-                  <tr key={rowKey} className='d-flex justify-content-center'>
-                    {
-                        tds.map((cell, cellIndex) => {
-                          return (
-
-                            <td key={cellIndex} className={
-                              rowKey !== targetRow
-                                ? ''
-                                : displayedColumns.includes(cellIndex) ? 'show' : 'hidden'
-                            }>
-                              <div className='position-relative'>
-                                {cell.imgDetails ? <div> {cell.imgDetails} </div> : <div className={`category-box ${cell.classColor}`} />}
-                                <div className="overlay">
-                                  <p className='td-font'>{cell.p}</p>
-                                </div>
-                              </div>
-                            </td>
-                          );
-                        })
-                      }
-                  </tr>
-                );
-              })}
-            </tbody>
-
-          </table>
-        </div>
-        <div className="w-100 d-flex justify-content-center mt-3 mb-5 scroll-btn-container">
-          <div className="scroll-buttons d-flex justify-content-between align-items-center">
-            <i className="fas fa-arrow-left px-3" style={{ color: 'rgb(110, 133, 178, 56%)', height: '100%' }} onClick={scrollLeft} />
-            <p className='scroll-btn-font p-0 m-0'>Scroll horizonally to see more</p>
-            <i className="fas fa-arrow-right px-3" style={{ color: 'rgb(110, 133, 178, 56%)', height: '100%' }} onClick={scrollRight} />
-          </div>
-        </div>
-      </>
-  );
-*/
