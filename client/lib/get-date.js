6921fce7aed53a1e9e26db46499ede504a890270
Hannah Engelhardt <hannahnengel@gmail.com>
2023-08-24 02:06:49 +0000

    Made getting the date a call to the server endpoint and updated the function

--- client/lib/get-date.js
+++ client/lib/get-date.js
@@ -1,9 +1,12 @@
 export default function getDate() {
   const date = new Date();
+  const options = { timeZone: 'America/Los_Angeles' };
+  const pstDate = date.toLocaleString('en-US', options);
+  const dateArray = pstDate.split(/[/,]/);
   const today = {
-    month: (date.getMonth() + 1),
-    date: date.getDate(),
-    year: date.getFullYear()
+    month: dateArray[0],
+    date: dateArray[1],
+    year: dateArray[2]
   };
   return (today);
 }
