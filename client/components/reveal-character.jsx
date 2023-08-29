af55722ada0b19cdc2e60089dab6387d3e055eb3
Hannah Engelhardt <hannahnengel@gmail.com>
2023-08-29 19:06:59 +0000

    Update Reveal component to include the countdown after the user wins, loses or forfeits

--- client/components/reveal-character.jsx
+++ client/components/reveal-character.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Countdown from './countdown';
 
 export default class RevealCharacter extends React.Component {
   constructor(props) {
@@ -45,47 +46,49 @@ export default class RevealCharacter extends React.Component {
     }
 
     return (
-      <div className="results-container mt-5 w-100">
-        <h1 className={`${titleClass} mt-3`}>{title}</h1>
-        <div className="row d-flex justify-content-center">
-          {imgDetails}
-        </div>
-        <div className="row d-flex justify-content-center">
-          <p className='m-0'>{characterOfTheDay.name}</p>
-        </div>
-        <div className="row d-flex justify-content-center">
-          <p className="guesses-font">
-            Attempts to Manage Mischeif: <span className="guesses-font">{colorMap.length}</span>
-          </p>
-        </div>
-        <div className="results-table-container">
-          <table className={`results-table ${tableMargin}`}>
-            <tbody>
-              {reversedColorMap
-                .slice(0, rowsExpanded ? colorMap.length : 5)
-                .map((guess, index) => {
-                  return (
-                    <tr key={guess.guessNumber} className="d-flex justify-content-center results-tr">
-                      {guess.colors.map((color, index) => {
-                        return (
-                          <td key={index} className="results-td">
-                            <div className={`results-category-box ${color.color}`} />
-                          </td>
-                        );
-                      })}
-                    </tr>
-                  );
-                })}
-            </tbody>
-          </table>
-          {colorMap.length > 5 && (
+      <>
+        <div className="results-container mt-5 mb-4 w-100">
+          <h1 className={`${titleClass} mt-3`}>{title}</h1>
+          <div className="row d-flex justify-content-center">
+            {imgDetails}
+          </div>
+          <div className="row d-flex justify-content-center">
+            <p className='m-0'>{characterOfTheDay.name}</p>
+          </div>
+          <div className="row d-flex justify-content-center">
+            <p className="guesses-font">
+              Attempts to Manage Mischeif: <span className="guesses-font">{colorMap.length}</span>
+            </p>
+          </div>
+          <div className="results-table-container">
+            <table className={`results-table ${tableMargin}`}>
+              <tbody>
+                {reversedColorMap
+                  .slice(0, rowsExpanded ? colorMap.length : 5)
+                  .map((guess, index) => {
+                    return (
+                      <tr key={guess.guessNumber} className="d-flex justify-content-center results-tr">
+                        {guess.colors.map((color, index) => {
+                          return (
+                            <td key={index} className="results-td">
+                              <div className={`results-category-box ${color.color}`} />
+                            </td>
+                          );
+                        })}
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </table>
+            {colorMap.length > 5 && (
             <button onClick={this.toggleRows} className="btn-font link mb-5">
               {rowsExpanded ? <><i className="fa-solid fa-arrows-up-to-line" /> Collapse</> : <><i className="fa-solid fa-arrows-down-to-line" /> Expand</>}
             </button>
-          )}
+            )}
+          </div>
         </div>
-
-      </div>
+        <Countdown />
+      </>
 
     );
   }
